6b04eecc6afe66f6c72b45c5428db6cf8c67fdff
Giovanni Bologni <giovannibologni@gmail.com>
2016-11-11 19:31:39 +0100

    Real-time logger on Excel
    * first ECG test
    * first try with low pass filter

--- arduino_serial_logger.ino
+++ arduino_serial_logger.ino
@@ -2,8 +2,25 @@
  SENDS CONDUCTANCE AND RESISTANCE VALUE TO THE SERIAL LINE.
  THEN THERE'S AN OFFICE' EXCEL PLUGIN TO READ THE DATA FROM THE SERIAL LINE
  AND STORE IT IN A TABLE. */
+ 
+#include <FilterDerivative.h>
+#include <FilterOnePole.h>
+#include <Filters.h>
+#include <FilterTwoPole.h>
+#include <FloatDefine.h>
+#include <RunningStatistics.h> //FILTERS LIBRARY
 
 #include <eHealth.h>
+
+float conductance, resistance, conductanceVol, ECG;
+float ECG_filtered;
+// filters out changes faster that 5 Hz.
+float filterFrequency = 5.0  ;
+
+
+// create a one pole (RC) lowpass filter
+  FilterOnePole lowpassFilter(LOWPASS, filterFrequency);   
+
 
 void setup() {
 
@@ -13,26 +30,40 @@ Serial.begin(9600);
   }
 Serial.println("CLEARDATA"); //clears up any data left from previous projects
 
-Serial.println("LABEL, Time, Conductance, Resistance, ..."); //always write LABEL, so excel knows the 
+Serial.println("LABEL, Time, Conductance, Resistance, ECG, ECG_filtered, ..."); //always write LABEL, so excel knows the 
 //next things will be the names of the columns (instead of Acolumn you could write Time for instance)
 
 Serial.println("RESETTIMER"); //resets timer to 0
 
  // establishContact();  // send a byte to establish contact until receiver responds
+ 
 
 }
 
 void loop() {
   
-  float conductance = eHealth.getSkinConductance();
-  float resistance = eHealth.getSkinResistance();
-  float conductanceVol = eHealth.getSkinConductanceVoltage();
+  //get the values from the sensors
+  
+  conductance = eHealth.getSkinConductance();
+  resistance = eHealth.getSkinResistance();
+  conductanceVol = eHealth.getSkinConductanceVoltage();
+  ECG = eHealth.getECG();
+ 
+  lowpassFilter.input( ECG ); //low pass filter on ECG signal
+
+  ECG_filtered = lowpassFilter.output(); 
+
+  //Serial.println(ECG);
 
   Serial.print("DATA, TIME , "); //writes the time in the first column A and the time since the measurements started in column B
   Serial.print(conductance); //INSERT THE DATA
   Serial.print(",");
   Serial.print(resistance); //INSERT THE DATA
+  Serial.print(",");
+  Serial.print(ECG, 2);
+   Serial.print(",");
+  Serial.print(ECG_filtered, 2);
   Serial.println("\n"); //be sure to add println to the last command so it knows to go into the next row on the second run
 
-  delay(100); //add a delay
+  delay(1); //add a delay
 }
