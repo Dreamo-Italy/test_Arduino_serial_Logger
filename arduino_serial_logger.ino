a8caffafbf15e891e2baaa4102dc82bd316318be
Giovanni Bologni <giovannibologni@gmail.com>
2016-11-14 00:01:55 +0100

    Update arduino_serial_logger.ino

--- arduino_serial_logger.ino
+++ arduino_serial_logger.ino
@@ -1,6 +1,6 @@
 /* THIS SKETCH GETS THE DATA FROM THE GALVANIC SKIN RESPONSE SENSOR AND
- SENDS CONDUCTANCE AND RESISTANCE VALUE TO THE SERIAL LINE.
- THEN THERE'S AN OFFICE' EXCEL PLUGIN TO READ THE DATA FROM THE SERIAL LINE
+ SENDS CONDUCTANCE AND RESISTANCE VALUES TO THE SERIAL LINE.
+ THERE'S AN "OFFICE EXCEL" PLUGIN TO READ THE DATA FROM THE SERIAL LINE
  AND STORE IT IN A TABLE. */
  
 #include <FilterDerivative.h>
@@ -8,14 +8,14 @@
 #include <Filters.h>
 #include <FilterTwoPole.h>
 #include <FloatDefine.h>
-#include <RunningStatistics.h> //FILTERS LIBRARY
+#include <RunningStatistics.h> //FILTER LIBRARIES
 
 #include <eHealth.h>
 
 float conductance, resistance, conductanceVol, ECG;
 float ECG_filtered;
-// filters out changes faster that 5 Hz.
-float filterFrequency = 5.0  ;
+
+float filterFrequency = 5.0  ; // filter out changes faster that 5 Hz.
 
 
 // create a one pole (RC) lowpass filter
@@ -28,15 +28,11 @@ Serial.begin(9600);
  while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-Serial.println("CLEARDATA"); //clears up any data left from previous projects
-
-Serial.println("LABEL, Time, Conductance, Resistance, ECG, ECG_filtered, ..."); //always write LABEL, so excel knows the 
-//next things will be the names of the columns (instead of Acolumn you could write Time for instance)
-
-Serial.println("RESETTIMER"); //resets timer to 0
-
- // establishContact();  // send a byte to establish contact until receiver responds
  
+Serial.println("CLEARDATA"); //clears up any data left from previous projects
+Serial.println("LABEL, Time, Conductance, Resistance, ECG, ECG_filtered, ...");  //always write LABEL, so excel knows the 
+                                                                                //next things will be the names of the columns 
+Serial.println("RESETTIMER"); //resets timer to 0 
 
 }
 
@@ -49,21 +45,26 @@ void loop() {
   conductanceVol = eHealth.getSkinConductanceVoltage();
   ECG = eHealth.getECG();
  
-  lowpassFilter.input( ECG ); //low pass filter on ECG signal
+ //low pass filter on ECG signal
+  lowpassFilter.input( ECG ); 
 
   ECG_filtered = lowpassFilter.output(); 
 
-  //Serial.println(ECG);
-
+  // Serial.println(ECG);           //print ECG value for debugging
+  // Serial.println(ECG_filtered);  //print ECG_filtered value for debugging
+ 
+ 
+//send the data to excel
+ 
   Serial.print("DATA, TIME , "); //writes the time in the first column A and the time since the measurements started in column B
-  Serial.print(conductance); //INSERT THE DATA
+  Serial.print(conductance);  //writes the CONDUCTANCE in the third column
   Serial.print(",");
-  Serial.print(resistance); //INSERT THE DATA
+  Serial.print(resistance);
   Serial.print(",");
   Serial.print(ECG, 2);
    Serial.print(",");
   Serial.print(ECG_filtered, 2);
-  Serial.println("\n"); //be sure to add println to the last command so it knows to go into the next row on the second run
+  Serial.println("\n"); //go into the next row on the next run
 
   delay(1); //add a delay
 }
